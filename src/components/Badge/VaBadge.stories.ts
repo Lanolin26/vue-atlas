12c77c21f2b7d7711b8dcf39219f77a4880d5a87
Lanolin26 <artemka250444@gmail.com>
2024-12-07 16:04:00 +0300

    WIP: Badge component with docs

--- src/components/Badge/VaBadge.stories.ts
+++ src/components/Badge/VaBadge.stories.ts
@@ -1,27 +1,30 @@
 import type { Meta, StoryFn } from "@storybook/vue3";
 import VaBadge from "./VaBadge.vue";
 
+type typeOfComponent = typeof VaBadge;
 
-type typeOfComponent = typeof VaBadge
+const body: string = "Body content"
 
 const meta: Meta<typeOfComponent> = {
-  title: "Skelet",
+  title: "Vue Atlas/VaBadge",
   component: VaBadge,
   argTypes: {
-    /*
-      placement?: string;
-      title?: string;
-      header?: boolean;
-      width?: string;
-     */
+    margin: {
+      type: "string",
+      control: { type: "text" },
+      description: "Поле, которое будет нанесено на внешнюю сторону элемента бейджа",
+    },
+    type: {
+      type: "string",
+      control: { type: "select" },
+      description: "Стиль значка для визуализации",
+      options: ["default", "primary", "success", "info", "warning", "danger"],
+    },
   },
   args: {
-    /*
-      placement?: string;
-      title?: string;
-      header?: boolean;
-      width?: string;
-     */
+    type: "default",
+    margin: "0px",
+    default: body,
   },
 };
 export default meta;
@@ -33,7 +36,22 @@ export const Default: StoryFn<typeOfComponent> = (args) => ({
   },
   template: `
     <div style="">
-
+      <va-badge v-bind="args">{{ args.default }}</va-badge>
     </div>
   `,
 });
+
+export const AllTypes: StoryFn<typeOfComponent> = () => ({
+  components: { VaBadge },
+  setup() {
+    const types = ["default", "primary", "success", "info", "warning", "danger"];
+    return {types};
+  },
+  template: `
+    <div>
+      <va-badge v-for="t in types" :type="t" margin="1px">
+        {{ t }}
+      </va-badge>
+    </div>
+  `
+})
